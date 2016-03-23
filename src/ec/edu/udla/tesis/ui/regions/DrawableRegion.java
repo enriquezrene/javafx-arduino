2f5ea3b18c46532bf88d1579a1fac376db393f2e
Rene Enriquez <renriquez@ioet.com>
2016-03-23 17:33:50 -0500

    test screenfw

---
+++ src/ec/edu/udla/tesis/ui/regions/DrawableRegion.java
@@ -0,0 +1,6 @@
+package ec.edu.udla.tesis.ui.regions;
+
+public interface DrawableRegion {
+
+	public void setContainer(RegionsContainer container);
+}
---
+++ src/screensframework/ControlledScreen.java
@@ -0,0 +1,8 @@
+package screensframework;
+
+
+public interface ControlledScreen {
+
+
+    public void setScreenParent(ScreensController screenPage);
+}
