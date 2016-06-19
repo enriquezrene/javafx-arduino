31c4d917f2659ae50547d3271281f96d6efe3035
Rene Enriquez <enriquezrene.ap@gmail.com>
2016-06-18 22:15:20 -0500

    timezone

--- src/ec/edu/udla/domain/dao/LecturaGlucometroDao.java
+++ src/ec/edu/udla/domain/dao/LecturaGlucometroDao.java
@@ -1,7 +1,11 @@
 package ec.edu.udla.domain.dao;
 
 import java.sql.Types;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
@@ -17,6 +21,18 @@ public class LecturaGlucometroDao extends AbstractDao {
         String sql = "SELECT * FROM lectura_glucometro ORDER BY id";
         List<LecturaGlucometro> lecturas = conexion.getJdbcTemplate().query(sql,
                 new BeanPropertyRowMapper<LecturaGlucometro>(LecturaGlucometro.class));
+        for (LecturaGlucometro lectura:
+             lecturas) {
+            SimpleDateFormat isoFormat = new SimpleDateFormat("yyy-MM-dd'T'HH:mm:ss");
+            String fecha = isoFormat.format(lectura.getFecha());
+            isoFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+            try {
+                Date date= isoFormat.parse(fecha);
+                lectura.setFecha(isoFormat.parse(fecha));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
         return lecturas;
     }
 
